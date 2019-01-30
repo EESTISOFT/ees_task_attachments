c6ebeaf4f73c14b7e964fa6530f5e4b34499758b
Tiago Magrini Rigo <tiagomrigo@gmail.com>
2019-01-30 15:01:16 -0200

    Add files via upload

---
+++ ees_task_attachments/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'EESTISOFT Task Attachments',
+    'version': '1.0',
+    'category': 'Addon',
+    'sequence': 1,
+    'summary': "Add attachments to project tasks",
+    'description': "EESTISOFT module which adds 'attachments' feature to tasks and sub-tasks in Project.",
+    'author': 'EESTISOFT, ' 'Mateus Cappellari Vieira',
+    'website': '',
+    'depends': ['base','project'],
+    'data': [
+        'views/doc_button.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'qweb': [],
+}
